4f2128228d6b69478ef85485ef9534f5d762c614
Oscar Rencoret <oscar.rencoret@alumnos.usm.cl>
2015-04-29 20:56:22 -0300

    Cambiando de PC

--- Client/UDPClient.java
+++ Client/UDPClient.java
@@ -3,6 +3,11 @@ import java.net.*;
 
 class UDPClient{
    private boolean conected;
+   private boolean loggedIn;
+
+   BufferedReader inByUser;
+
+   private InetAddress serverIP;
 
    public static void main(String args[]) throws Exception{
       UDPClient clienteFTP = new UDPClient();
@@ -11,21 +16,69 @@ class UDPClient{
    }
 
    private void mainMenu() throws Exception{
+      String response = "";
+
       System.out.println("Bienvenido al cliente de servidor FTP LocalHost, de Dribyte!");
-      BufferedReader inByUser = new BufferedReader(new InputStreamReader(System.in));
+      this.inByUser = new BufferedReader(new InputStreamReader(System.in));
       String input = "0";
-      System.out.println("Ingrese un comando (Para obtener informacion de los comandos disponibles tipee Help)");
+      System.out.println("Ingrese un comando (Para obtener informacion de los comandos disponibles tipee HELP)");
       while(!input.equals("QUIT")){
          System.out.print("$ ");
-         input = inByUser.readLine();
-         if(input.equals("Help"))
+         input = this.inByUser.readLine();
+         if(input.equals("HELP"))
             printHelp();
-         else if(input.equals("QUIT"))
+         else if(input.equals("QUIT")){
+            response = send(input);
             System.out.println("Conexion terminada, cerrando programa\nAdios!");
-         else if(input.equals("OPEN") || (input.equals("CD") || input.equals("LS") || input.equals("GET") || input.equals("PUT") && conected))
-            server(input);
+         }
+         else if(input.startsWith("OPEN")){
+            if(!loggedIn){
+               try{
+                  String[] split = input.split(" ");
+                  if(split[1].startsWith("localhost"))
+                     this.serverIP = InetAddress.getByName("localhost");
+                  else
+                     this.serverIP = InetAddress.getByName(split[1]);
+                  input = split[0];
+                  response = send(input);
+                  String[] responses = response.split("-");
+                  System.out.println(responses[1]);
+
+                  if(responses[0].equals("220")){
+                     this.conected = true;
+                     logIn();
+                     continue;
+                  }
+                  else{
+                     response = "No contestado por el servidor??";
+                  }
+               }
+               catch(Exception e){
+                  System.out.println("Error en el comando");
+                  System.out.println("Recuerde: OPEN <ip>");
+               }
+            }
+            else{
+               System.out.println("Ya tiene la sesión iniciada en el servidor");
+            }
+         }
+         else if(!this.conected){
+            System.out.println("No se encuentra conectado al servidor, intente con OPEN");
+         }
+         else if(input.startsWith("CD")){
+            response = send(input);
+         }
+         else if(input.startsWith("LS")){
+            response = send(input);
+         }
+         else if(input.startsWith("GET")){
+            fileGet(input);
+         }
+         else if(input.startsWith("PUT")){
+         }
          else
             System.out.println("Operacion no encontrada: " + input);
+         System.out.println(response);
       }
    }
 
@@ -39,15 +92,14 @@ class UDPClient{
       System.out.println("QUIT -> Para cerrar la conexion y salir del sistema\n");
    }
 
-   private void server(String message) throws Exception{
+   private String send(String message) throws Exception{
       DatagramSocket clientSocket = new DatagramSocket();
-      InetAddress IPAddress = InetAddress.getByName("localhost");
 
       byte[] sendData = new byte[1024];
       byte[] receiveData = new byte[1024];
 
       sendData = message.getBytes();
-      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 21);
+      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, this.serverIP, 21);
       clientSocket.send(sendPacket);
 
       DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
@@ -56,12 +108,40 @@ class UDPClient{
       String response = new String(receivePacket.getData());
       response = response.trim();
 
-      String[] responses = response.split("-");
+      return response;
+   }
 
-      if(responses[0].equals("220"))
-         conected = true;
+   private void logIn() throws Exception{
+      String response;
+      String[]  split;
+      String in;
 
-      System.out.println(responses[1]);
-      clientSocket.close();
+      System.out.print("Ingrese Usuario: ");
+      in = inByUser.readLine();
+      response = send(in);
+      split = response.split("-");
+      if(!split[0].equals("331")){
+         System.out.println(response);
+         System.out.println("Usuario incorrecto intente OPEN nuevamente!");
+         return;
+      }
+      System.out.println(response);
+      System.out.print("Ingrese Contraseña: ");
+      in = "";
+      in = inByUser.readLine();
+      response = send(in);
+      split = response.split("-");
+      if(!split[0].equals("230")){
+         System.out.println(response);
+         System.out.println("Password incorrecta intente OPEN nuevamente!");
+         return;
+      }
+      this.loggedIn = true;
+      System.out.println(response);
+      return;
+   }
+
+   private void fileGet(String input){
+      response = send(input);
    }
 }
--- Server/UDPServer.java
+++ Server/UDPServer.java
@@ -4,38 +4,186 @@ import java.net.*;
 class UDPServer{
    private boolean loggedIn;
 
+   private DatagramSocket controlSocket;
+   private DatagramSocket transferSocket;
+
+   private String path;
+   private String realPath;
+
    public static void main(String args[]) throws Exception{
       UDPServer server = new UDPServer();
       server.loggedIn = false;
+      server.controlSocket = new DatagramSocket(20);
+      server.transferSocket = new DatagramSocket(21);
+      server.path = "/home/admin/";
+      server.realPath = "";
       server.listenMessage();
    }
 
    private void listenMessage() throws Exception{
-      DatagramSocket serverSocket = new DatagramSocket(21);
       byte[] receiveData = new byte[1024];
-      byte[] sendData = new byte[1024];
       while(true){
+         receiveData = new byte[1024];
          DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-         serverSocket.receive(receivePacket);
-         String message = new String(receivePacket.getData());
-         message = message.trim();
-         String response = response(message);
-         InetAddress ipAddress = receivePacket.getAddress();
-         int port = receivePacket.getPort();
-         sendData = response.getBytes();
-         DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, port);
-         serverSocket.send(sendPacket);
+         this.transferSocket.receive(receivePacket);
+         this.response(receivePacket);
       }
    }
 
    private void listenFile(){
    }
 
-   private String response(String message){
-      if(message.equals("OPEN"))
-         return "220-Hola soy el servidor FTP de Dribyte!\nPor favor inicie sesion para acceder a las funcionalidades del servidor";
+   private void response(DatagramPacket receivePacket) throws Exception{
+      byte[] sendData = new byte[1024];
+
+      String message = new String(receivePacket.getData());
+      message = message.trim();
+
+      String answer = "";
+
+      if(message.startsWith("OPEN")){
+         if(!loggedIn){
+            logIn(receivePacket);
+            return;
+         }
+         else
+            answer = "230-Ya se inicio sesion en el servidor!";
+      }
+      else if(!loggedIn){
+         answer = "530-No se ha iniciado sesion aun";
+      }
+      else if(message.startsWith("CD")){
+         answer = changeDirectory(message);
+      }
+      else if(message.startsWith("LS")){
+         answer = listDirectory();
+      }
+      else if(message.startsWith("GET")){
+         fileSend(message);
+      }
       else
-         return "501-No entendi ._.";
+         answer = "501-No entendi ._.";
+
+      InetAddress ipAddress = receivePacket.getAddress();
+      int port = receivePacket.getPort();
+      sendData = answer.getBytes();
+      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, port);
+      this.controlSocket.send(sendPacket);
    }
 
+   private void logIn(DatagramPacket receivePacket) throws Exception{
+      byte[] receiveData = new byte[1024];
+      byte[] sendData = new byte[1024];
+      String answer;
+      boolean flag;
+
+      //Preparando el inicio de sesion
+      answer = "220-Hola soy el servidor FTP de Dribyte!\nPor favor inicie sesion para acceder a las funcionalidades del servidor";
+      InetAddress ipAddress = receivePacket.getAddress();
+      int port = receivePacket.getPort();
+      sendData = answer.getBytes();
+      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, port);
+      this.controlSocket.send(sendPacket);
+
+      receiveData = new byte[1024];
+      sendData = new byte[1024];
+
+      //Recibiendo el usuario
+      flag = false;
+      receivePacket = new DatagramPacket(receiveData, receiveData.length);
+      this.transferSocket.receive(receivePacket);
+      String user = new String(receivePacket.getData());
+      user = user.trim();
+      if(!user.equals("admin")){
+         answer = "430-Usuario Invalido!";
+         flag = true;
+      }
+      else
+         answer = "331-Usuario Correcto!";
+      ipAddress = receivePacket.getAddress();
+      port = receivePacket.getPort();
+      sendData = answer.getBytes();
+      sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, port);
+      this.controlSocket.send(sendPacket);
+      if(flag)
+         return;
+
+      receiveData = new byte[1024];
+      sendData = new byte[1024];
+
+      //Recibiendo la password
+      receivePacket = new DatagramPacket(receiveData, receiveData.length);
+      this.transferSocket.receive(receivePacket);
+      String password = new String(receivePacket.getData());
+      password = password.trim();
+      if(!password.equals("1234")){
+         answer = "430-Contraseña incorrecta!";
+      }
+      else{
+         answer = "230-Inicio de Sesion Completado!";
+         this.loggedIn = true;
+      }
+      ipAddress = receivePacket.getAddress();
+      port = receivePacket.getPort();
+      sendData = answer.getBytes();
+      sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, port);
+      this.controlSocket.send(sendPacket);
+      return;
+   }
+
+   private String changeDirectory(String message){
+      String[] split = message.split(" ");
+      if(split[1].equals("..")){
+         String[] splitPath = this.path.split("/");
+         String[] splitRealPath = this.realPath.split("/");
+         this.path = "/home/admin/";
+         this.realPath = "";
+         if(splitRealPath.length > 2){
+            for(int i = 0; i < splitPath.length - 2; i++){
+               this.path += splitPath[i];
+            }
+            for(int i = 0; i < splitRealPath.length - 2; i++){
+               this.realPath += splitRealPath[i];
+            }
+         }
+         return "250-El nuevo directorio actual es: '" + this.path + "'";
+      }
+
+      File folder = new File(realPath + split[1]);
+      if(folder.exists()){
+         this.path += (split[1] + "/");
+         this.realPath += (split[1] + "/");
+         return "250-El nuevo directorio actual es: '" + this.path + "'";
+      }
+      else{
+         return "550-Directorio no encontrado: " + split[1];
+      }
+   }
+
+   private String listDirectory(){
+      String listOfFiles = "";
+      String localPath;
+
+      if(realPath.equals(""))
+         localPath = ".";
+      else
+         localPath = realPath;
+
+      try{
+         File folder = new File(localPath);
+         listOfFiles = "250-Lista de Archivos\n";
+         File[] files = folder.listFiles();
+         for(File file: files){
+            listOfFiles += file.getName() + "\n";
+         }
+      }
+      catch(Exception e){
+         return "550-Problemas en el servidor";
+      }
+      return listOfFiles;
+   }
+
+   private void fileSend(String message){
+      
+   }
 }
